3adf8ec4c5cec0f1ebce31ec8bffe07a1b9268e5
cymbah <future.simke96@gmail.com>
2023-08-14 07:47:59 -0700

    try

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 15; i++)
+	{
+		putchar(i < 10 ? i + '0' : i - 10 + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
