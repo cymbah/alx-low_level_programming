ed01a923c15c312a8b40efce2558d0410577c09d
cymbah <future.simke96@gmail.com>
2023-08-14 12:29:24 -0700

    try

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10;j++)
+		{
+			if (i != j)
+			{
+				putchar(i + '0');
+				putchar(',');
+				putchar(j + '0');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
