24bac3a0e598212316ba34b93afeffdd8d77403c
cymbah <future.simke96@gmail.com>
2023-08-15 12:05:04 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * main - Entry point.
- * Return: 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar(' ');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
