28e6b178c7fbbfe33a5a4dc64bcbb9820635a1d4
cymbah <future.simke96@gmail.com>
2023-10-31 12:17:05 -0700

    corrections

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,49 +1,44 @@
-include "main.h"
-
-
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* alloc_grid - returns a pointer to a
-*2 dimensional array of integers.
-*@width: width oàf array
-*@height: height of array
-*
-*Return: pointer to the array
-*/
+ * alloc_grid - function that returns a pointer to a 2 dimensional array of int
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: address of the newly allocated memory
+ */
 
 int **alloc_grid(int width, int height)
 {
-	int **array;
-	int i = 0, j;
+	int i, j, m, n;
+	int **grid;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = (int **) malloc(sizeof(int *) * height);
-	if (array != NULL)
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
 	{
-		for (; i < height; i++)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
-			array[i] = (int *) malloc(sizeof(int) * width);
-			if (array[i] != NULL)
+			for (j = 0; j < i; j++)
 			{
-				for (j = 0; j < width; j++)
-					array[i][j] = 0;
+				free(grid[j]);
 			}
-			else
-			{
-				while (i >= 0)
-				{
-					free(array[i]);
-					i--;
-				}
-				free(array);
-				return (NULL);
-			}
+			free(grid);
+			return (NULL);
 		}
-		return (array);
 	}
-	else
+	for (m = 0; m < height; m++)
 	{
-		return (NULL);
+		for (n = 0; n < width; n++)
+			grid[m][n] = 0;
 	}
+	return (grid);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* free_grid - frees a 2d array
-*@grid:memory block to be freed
-*@height:height of the array
-*Return:returns void
-*/
+ * free_grid - function that frees a 2 dimensional grid
+ * previously created by your alloc_grid function
+ *
+ * @grid: double pointer to the grid
+ * @height: height of the grid
+ *
+ * Return: address of the newly allocated memory
+ */
 
 void free_grid(int **grid, int height)
 {
-	int i;
+	int j;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-
+	for (j = 0; j < height; j++)
+	{
+		free(grid[j]);
+	}
 	free(grid);
 }
